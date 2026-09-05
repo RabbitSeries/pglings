d8c93b333d0f54782ef630d066412a9cbe2b3d1c
RabbitSeries <xiali123.ch@gmail.com>
2026-09-05 15:40:50 +0800

    day 06

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #! /bin/sh
-# Options: -v ON_ERROR_ROLLBACK=off, which is off by default, this is counter intuitive,
+# Options: -v ON_ERROR_ROLLBACK=off, which is off by default, this is counter-intuitive,
 # it should be called ON_ERROR_CONTINUE, when set to 'off' the connection will exit
-# and a rollback to some implicit savepoint is performed.
+# if a statement in a transaction block generates error and rollback to some implicit
+# savepoint just before each command in the transaction block.
+# So make sure each write statement is in a transaction block.
+
 # Options: -1, --single-transaction, the entire connection will be wrapped into a
 # BEGIN; COMMIT; clause.
 # Options: -v ON_ERROR_STOP=1
-psql -f $1 -d exercises -v ON_ERROR_STOP=1 | tee psql.log
+psql -f $1 -d exercises -v ON_ERROR_STOP=on | tee psql.log
