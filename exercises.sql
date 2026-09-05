9e67e70526864098ccadf486747ccf2c3bc543c3
RabbitSeries <xiali123.ch@gmail.com>
2026-09-05 15:40:50 +0800

    day 06

--- exercises.sql
+++ exercises.sql
@@ -77,7 +77,7 @@ LIMIT 10;
 
 SELECT DISTINCT surname
 FROM cd.members
-UNION
+UNION ALL
 SELECT DISTINCT name
 FROM cd.facilities;
 
@@ -398,18 +398,395 @@ FROM
 SELECT COUNT(DISTINCT memid)
 FROM cd.bookings;
 
+-- Unfortunately, Postgres, unlike some other RDBMSs like SQL Server and MySQL,
+-- doesn't support putting column names in the HAVING clause.
+-- So, reference to alias is not allowed in neither WHERE and HAVING clause, in which
+-- HAVING clause filters result after GROUP BY while WHERE clause filters the input.
+-- Both HAVING and WHERE clause are evaluated before SELECT's alias statement.
+-- Note that ORDER BY can also use other comparable expressions and aggregate functions
+-- if there is a aggregation statement in the query.
 
 SELECT facid,
        SUM(slots) AS "Total Slots"
 FROM cd.bookings
 GROUP BY facid
-HAVING "Total Slots" > 1000
+HAVING SUM(slots) > 1000 -- HAVING "Total Slots" > 1000
 ORDER BY facid ASC;
 
+-- Yet, it seems to be undocumented that whether non-volatile functions'
+-- result is cached or not,
+-- Just use another subquery or use the power of CTEs (Common Table Expressions).
+
+SELECT *
+FROM
+    (SELECT facid,
+            SUM(slots) AS "Total Slots"
+     FROM cd.bookings
+     GROUP BY facid)
+WHERE "Total Slots" > 1000
+ORDER BY facid ASC;
+
+-- Guest when mems.memid = 0.
+
+SELECT
+    (SELECT name
+     FROM cd.facilities fac
+     WHERE fac.facid = expenses.facid), SUM(expense) AS revenue
+FROM
+    (SELECT bks.facid as facid,
+            SUM(bks.slots) * (
+                                  (SELECT CASE
+                                              WHEN mems.memid = 0 THEN "guestcost"
+                                              ELSE "membercost"
+                                          END
+                                   FROM cd.facilities fac
+                                   WHERE fac.facid = bks.facid)) as expense
+     FROM cd.members mems
+     JOIN cd.bookings bks ON mems.memid = bks.memid
+     GROUP BY mems.memid,
+              bks.facid) expenses
+GROUP BY facid
+ORDER BY revenue; -- This also works: ORDER BY SUM(expense);
+
+-- Can be simplified into the following query. If no extra columns from cd.member
+-- besides the constraints are needed, don't reference it.
+-- This is not allowed, ungrouped column nust be used in aggregator functions.
+--    SUM(slots) * CASE
+--                    WHEN memid = 0 THEN guestcost
+--                    ELSE membercost
+--                END
+
+SELECT name,
+       SUM(slots * CASE
+                       WHEN memid = 0 THEN guestcost
+                       ELSE membercost
+                   END) revenue
+FROM cd.facilities fac
+JOIN cd.bookings bks ON fac.facid = bks.facid
+GROUP BY name
+ORDER BY revenue;
+
+
+SELECT *
+FROM
+    (SELECT name,
+            SUM(slots * CASE
+                            WHEN memid = 0 THEN guestcost
+                            ELSE membercost
+                        END) revenue
+     FROM cd.facilities fac
+     JOIN cd.bookings bks ON fac.facid = bks.facid
+     GROUP BY name)
+WHERE revenue < 1000
+ORDER BY revenue;
+
+
+SELECT facid,
+       "Total Slots"
+FROM
+    (SELECT facid,
+            SUM(slots) AS "Total Slots"
+     FROM cd.bookings
+     GROUP BY facid)
+WHERE "Total Slots" =
+        (SELECT MAX("Total Slots")
+         FROM
+             (SELECT facid,
+                     SUM(slots) AS "Total Slots"
+              FROM cd.bookings
+              GROUP BY facid));
+
+-- Can be simplified into
 
 SELECT facid,
        SUM(slots) AS "Total Slots"
 FROM cd.bookings
 GROUP BY facid
-HAVING SUM(slots) > 1000
-ORDER BY facid ASC;
+ORDER BY "Total Slots" DESC
+LIMIT 1;
+
+-- Or in a CTE approach:
+WITH totalslots AS
+    (SELECT facid,
+            SUM(slots) AS "Total Slots"
+     FROM cd.bookings
+     GROUP BY facid)
+SELECT facid,
+       "Total Slots"
+FROM totalslots
+WHERE "Total Slots" =
+        (SELECT MAX("Total Slots")
+         FROM totalslots);
+
+-- Don't look at this shithole.
+WITH totalslots AS 
+    (SELECT facid, 
+            date_part('Month', starttime) AS "month", 
+            SUM(slots) AS "Total Slots" 
+     FROM cd.bookings 
+     WHERE date_part('Year', starttime) = 2012
+     GROUP BY facid,
+              "month"
+     ORDER BY facid,
+              "month"),
+     maxfacid AS
+    (SELECT (MAX(facid) + 1) as facid,
+            NULL as "month",
+            SUM("Total Slots")
+     FROM totalslots)
+SELECT (CASE
+            WHEN facid =
+                     (SELECT facid
+                      FROM maxfacid) THEN NULL
+            ELSE facid
+        END) AS facid,
+       (CASE
+            WHEN "month" = 255 THEN NULL
+            ELSE "month"
+        END) AS "month",
+       "Total Slots"
+FROM
+    (SELECT *
+     FROM totalslots
+     UNION
+         (SELECT facid,
+                 255 AS "month",
+                 SUM("Total Slots")
+          FROM totalslots
+          GROUP BY facid)
+     UNION
+         (SELECT
+              (SELECT facid
+               FROM maxfacid) as facid,
+                 NULL as "month",
+                 SUM("Total Slots")
+          FROM totalslots)
+     ORDER BY facid,
+              "month");
+
+-- This is better, NULL will be ranked as the last element.
+WITH totalslots AS
+    (SELECT facid,
+            date_part('Month', starttime) AS "month",
+            SUM(slots) AS "Total Slots"
+     FROM cd.bookings
+     WHERE date_part('Year', starttime) = 2012
+     GROUP BY facid,
+              "month")
+SELECT *
+FROM totalslots
+UNION ALL
+    (SELECT facid,
+            null,
+            SUM("Total Slots")
+     FROM totalslots
+     GROUP BY facid)
+UNION ALL
+    (SELECT NULL,
+            NULL,
+            SUM("Total Slots")
+     FROM totalslots)
+ORDER BY facid,
+         "month";
+
+-- This is the best.
+-- Empty list will aggregate all the rows.
+-- NULL for ungrouped and unaggregated columns.
+
+SELECT facid,
+       date_part('Month', starttime) AS "month",
+       SUM(slots) AS "Total Slots"
+FROM cd.bookings
+WHERE date_part('Year', starttime) = 2012
+GROUP BY GROUPING
+SETS ((facid,
+       "month"), (facid), ())
+ORDER BY facid,
+         "month";
+
+-- Which is equivalent to the following query.
+-- ROLLUP represents the given list of expressions and all prefixes of the list
+-- including the empty list
+
+SELECT facid,
+       date_part('Month', starttime) AS "month",
+       SUM(slots) AS "Total Slots"
+FROM cd.bookings
+WHERE date_part('Year', starttime) = 2012
+GROUP BY ROLLUP (facid,
+                 "month")
+ORDER BY facid,
+         "month";
+
+
+SELECT bks.facid,
+       "name",
+       round(SUM(slots) * 0.5, 2) as "Total Hours"
+FROM cd.bookings bks
+JOIN cd.facilities facs ON bks.facid = facs.facid
+GROUP BY bks.facid,
+         "name"
+ORDER BY bks.facid;
+
+-- Removing "name" also works if facs.facid is used.
+-- Other database management system might not support this.
+-- Don't use this for cross-platform compatability.
+
+SELECT facs.facid,
+       "name",
+       trunc(SUM(slots) * 0.5, 2) as "Total Hours"
+FROM cd.bookings bks
+JOIN cd.facilities facs ON bks.facid = facs.facid
+GROUP BY facs.facid
+ORDER BY facs.facid;
+
+
+SELECT mems.surname,
+       mems.firstname,
+       mems.memid,
+       MIN(starttime) AS starttime
+FROM cd.members mems
+JOIN cd.bookings bks ON mems.memid = bks.memid
+WHERE starttime >= '2012-9-1'
+GROUP BY mems.memid,
+         mems.surname,
+         mems.firstname
+ORDER BY mems.memid;
+
+
+SELECT COUNT(*) OVER(),
+                firstname,
+                surname
+FROM cd.members
+ORDER BY joindate;
+
+
+SELECT row_number() OVER(
+                         ORDER BY joindate),
+                    firstname,
+                    surname
+FROM cd.members;
+
+-- An aggregate used with ORDER BY and the default window frame definition
+-- produces a “running sum” type of behavior,
+-- which may or may not be what's wanted.
+-- So this will produce the same result as above, which is counter-intuitive.
+-- In this example the window is the same for each row, but each row's window
+-- frame is restricted into start row to current row.
+
+SELECT COUNT(*) OVER(
+                     ORDER BY joindate) AS row_number,
+       firstname,
+       surname
+FROM cd.members;
+
+
+SELECT facid,
+       total_slots
+FROM
+    (SELECT facid,
+            total_slots,
+            rank() OVER (
+                         ORDER BY total_slots DESC) "rank"
+     FROM
+         (SELECT facid,
+                 SUM(slots) total_slots
+          FROM cd.bookings
+          GROUP BY facid))
+WHERE "rank" = 1;
+
+-- Or resue the sum, as column number or name can't be referenced in OVER expression:
+
+SELECT facid,
+       total_slots
+FROM
+    (SELECT facid,
+            SUM(slots) total_slots,
+            rank() OVER (
+                         ORDER BY SUM(slots))
+     FROM cd.bookings
+     GROUP BY facid)
+WHERE "rank" = 1;
+
+-- The final result is sorted according to one of the ORDER BY statement
+-- in the OVER window expressions if no ORDER BY is specified in the
+-- outmost SELECT clause.
+
+SELECT *,
+       rank() OVER (
+                    ORDER BY "hours" DESC) "rank"
+FROM
+    (SELECT firstname,
+            surname,
+            round(SUM(slots) / 2.0, -1) "hours"
+     FROM cd.members mems
+     JOIN cd.bookings bks ON mems.memid = bks.memid
+     GROUP BY firstname,
+              surname,
+              mems.memid)
+ORDER BY "rank",
+         surname,
+         firstname;
+
+
+SELECT *
+FROM
+    (SELECT name,
+            rank() OVER (
+                         ORDER by revenue DESC) "rank"
+     FROM
+         (SELECT "name",
+                 SUM(slots * CASE
+                                 WHEN memid = 0 THEN guestcost
+                                 ELSE membercost
+                             END) AS revenue
+          FROM cd.bookings bks
+          JOIN cd.facilities fac ON bks.facid = fac.facid
+          GROUP BY "name"))
+WHERE "rank" <= 3
+ORDER BY name;
+
+-- OVER windows is also allowed to access grouped rows.
+-- The over window can also contain calculations.
+-- Thus the above query can be simplified into:
+
+SELECT *
+FROM
+    (SELECT name,
+            rank() OVER (
+                         ORDER by SUM(slots * CASE
+                                                  WHEN memid = 0 THEN guestcost
+                                                  ELSE membercost
+                                              END) DESC) "rank"
+     FROM cd.facilities fac
+     JOIN cd.bookings bks ON fac.facid = bks.facid
+     GROUP BY name)
+WHERE "rank" <= 3
+ORDER BY name;
+
+
+SELECT *
+FROM
+    (SELECT name,
+            ntile(3) OVER (
+                           ORDER by SUM(slots * CASE
+                                                    WHEN memid = 0 THEN guestcost
+                                                    ELSE membercost
+                                                END) DESC) "rank"
+     FROM cd.facilities fac
+     JOIN cd.bookings bks ON fac.facid = bks.facid
+     GROUP BY name)
+ORDER BY rank,
+         name;
+
+
+SELECT fac.name,
+       initialoutlay / (SUM(slots * CASE
+                                        WHEN memid = 0 THEN guestcost
+                                        ELSE membercost
+                                    END) / 3 - monthlymaintenance) AS revenue
+FROM cd.bookings bks
+JOIN cd.facilities fac ON bks.facid = fac.facid
+GROUP BY fac.facid,
+         name
+ORDER BY name;
